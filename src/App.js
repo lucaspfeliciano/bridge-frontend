7755796da1bc40fb677cb5cd6bf839220b120617
lucaspfeliciano <lucaspfeliciano@hotmail.com>
2020-03-18 20:47:04 -0300

    Adiciona base do projeto sem estilizações

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import api from './service/api';
+
+function App() {
+
+  const [number, setNumber] = useState('');
+  const[isPrime, setIsPrime] = useState('');
+  const[dividers, setDividers] = useState([]);
+
+    async function handleSubmit(event) {
+      event.preventDefault();
+      const response = await api.post('/divide', {number});
+      if(response.data.isPrime == true) {
+        setIsPrime('Sim');
+      } else {
+        setIsPrime('Não');
+      }
+      setDividers(response.data.dividers);
+    }
+
+  return (
+    <>
+            <p>
+            Insert a number
+            </p>
+
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="number">Number *</label>
+                <input 
+                    type="number" 
+                    id="number" 
+                    placeholder="Insert a number"
+                    value= {number}
+                    onChange={event => setNumber(event.target.value)}
+                />
+                <button className= "btn" type= "submit">Go</button>
+            </form>
+            {isPrime ? 
+            <>
+              <p> O número é primo? {isPrime}</p>
+              <p>Seus divisores são: {dividers.map(numero => <p>{numero}</p>)}</p>
+            </>
+             : 
+             ''}
+        </>
+  );
+}
+
+export default App;
